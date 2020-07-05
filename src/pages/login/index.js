4ececbaacb0fc089a534a0830d55dd5de2b0ae6b
Pedro Pacheco <domprechecopa@gmail.com>
2020-07-05 12:22:40 -0300

    Senha oculta

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, LogoImagem, LoginInput, ViewButaoEntrada,
     CreateConta, ButaoTextCreateConta, EntrarConta, ButaoTextEntrarConta,
     EsqueceuSenha, TextEsqueceuSenha } from './components';
 import Logo from '../../assets/logo-on-page.png';
 
 export default function Login(){
+    const [cpf, setCPF] = useState('');
+    const [senha, setSenha] = useState('senha');
+
     return(
         <Container>
             <LogoImagem source={Logo} />
-            <LoginInput placeholder="CPF" placeholderTextColor="#B7B7B7"></LoginInput>
-            <LoginInput placeholder="Senha" placeholderTextColor="#B7B7B7" style={{marginTop: 12}}></LoginInput>
+            <LoginInput placeholder="CPF" clearTextOnFocus={true} 
+            onChangeText={(text) => setCPF(text)} clearTextOnvalue={cpf}
+             placeholderTextColor="#B7B7B7" keyboardType='number-pad'></LoginInput>
+            <LoginInput placeholder="Senha" placeholderTextColor="#B7B7B7" style={{marginTop: 12}}
+              secureTextEntry={true} textContentType="password" ></LoginInput>
             
             <ViewButaoEntrada>
                 <CreateConta><ButaoTextCreateConta>Criar conta</ButaoTextCreateConta></CreateConta>
