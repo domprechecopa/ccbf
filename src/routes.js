149919573ddea79b5171e91fb5c1c837b56eecb4
Pedro Pacheco <domprechecopa@gmail.com>
2020-07-05 06:07:56 -0300

    Create Routes and Working

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './pages/login';
+
+const Stack = createStackNavigator();
+
+export default function Routes(){
+    return(
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" headerMode="none" options={{headerShown: false}}>
+            <Stack.Screen name="Login" component={Login}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+    );
+};
